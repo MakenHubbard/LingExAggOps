3eee99014604b713fae22a6eefc061ffb9e39a90
Maken Hubbard <makhub26@outlook.com>
2018-09-22 10:50:13 -0500

    adding the code for count, sum, and max methods using linq in collections of lists

--- LingExAggOps/Program.cs
+++ LingExAggOps/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LingExAggOps
 {
@@ -6,7 +8,38 @@ namespace LingExAggOps
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> Nums = new List<int>()
+            {
+                15, 8, 21, 24, 28, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var total = Nums.Count();
+
+            Console.WriteLine(total);
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+
+            List<double> Purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            double moneySpent = Purchases.Sum();
+
+            Console.WriteLine($"As a Company We have spent ${moneySpent} in this month!");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+
+            List<double> Prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            double mostPricey = Prices.Max();
+
+            Console.WriteLine($" The most expensive product costs ${mostPricey}");
+            Console.WriteLine("Press enter to end this application");
+            Console.ReadLine();
         }
     }
 }
